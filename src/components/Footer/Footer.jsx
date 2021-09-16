6c2c938be40809f0596f447648380d9d3789400b
Hafedh Amri <amri_hafedh@yahoo.com>
2021-09-16 11:52:40 +0200

    Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer className="text-center text-white fixed-bottom">
+  {/* Grid container */}
+  <div className="container p-4"></div>
+  {/* <!-- Grid container --> */}
+
+  {/*!-- Copyright --*/}
+  <div className="text-center p-3">
+    © 2020 Copyright:
+    <a href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  {/* <!-- Copyright --> */}
+</footer>
+    )
+}
+
+export default Footer
