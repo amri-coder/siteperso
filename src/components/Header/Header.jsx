5f9ba797dd28b8b749e8b5c063216abe34cdc2cb
Hafedh Amri <amri_hafedh@yahoo.com>
2021-09-16 11:33:25 +0200

    projet perso

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import {Container, Nav, NavDropdown} from 'react-bootstrap'
+
+const Header = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand href="#home">Amri Maher</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+      <Nav.Link href="#accueil">Accueil</Nav.Link>
+      <Nav.Link href="#pricing">À propos</Nav.Link>
+      <NavDropdown title="Services" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">Contact</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Réalisations
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+    )
+}
+
+export default Header
