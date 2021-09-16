ba8775f1e93fd6daf4eb263216176068d087d0d8
Hafedh Amri <amri_hafedh@yahoo.com>
2021-09-16 18:59:52 +0200

    projet perso

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,23 +10,23 @@ const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" className="navbar">
             <Container>
-                <Navbar.Brand><Link to="/Accueil">Amri Maher</Link></Navbar.Brand>
+                <Navbar.Brand><Link to="/Accueil" className='text-link'>Amri Maher</Link></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                   <Nav className="me-auto">
-                  <Nav.Link><Link to="/Accueil">Accueil</Link></Nav.Link>
-                  <Nav.Link><Link to="/About">À propos</Link></Nav.Link>
-                    <NavDropdown title="Services" id="collasible-nav-dropdown">
-                      <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                      <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                      <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <Nav.Link><Link to="/Accueil" className='text-link'>Accueil</Link></Nav.Link>
+                  <Nav.Link><Link to="/About" className='text-link'>À propos</Link></Nav.Link>
+                    <NavDropdown title="Services" id="collasible-nav-dropdown" className='text-link'>
+                      <NavDropdown.Item href="#action/3.1" className='text-link'>Action</NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.2" className='text-link'>Another action</NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.3" className='text-link'>Something</NavDropdown.Item>
                       <NavDropdown.Divider />
                       <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                     </NavDropdown>
                   </Nav>
                   <Nav>
-                  <Nav.Link><Link to="/Realisations">Réalisations</Link></Nav.Link>
-                  <Nav.Link><Link to="/Contact">Contact</Link></Nav.Link>
+                  <Nav.Link><Link to="/Realisations" className='text-link'>Réalisations</Link></Nav.Link>
+                  <Nav.Link><Link to="/Contact" className='text-link'>Contact</Link></Nav.Link>
                     
                   </Nav>
                 </Navbar.Collapse>
