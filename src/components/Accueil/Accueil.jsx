9b5712071e14cb0a0bd6df2b00108ede3c38bc25
Hafedh Amri <amri_hafedh@yahoo.com>
2021-09-19 13:03:24 +0200

    projet perso

--- src/components/Accueil/Accueil.jsx
+++ src/components/Accueil/Accueil.jsx
@@ -1,32 +1,48 @@
 import React from 'react';
 import './Accueil.css';
 import {Container, Row, Col} from 'react-bootstrap';
+import Typography from '@mui/material/Typography';
 
 
-const Accueil = () => {
+
+function Accueil (){
+    
     return (
         <div className="accueil">
 
             <Container>
             
                 <Row className="acc">
+
                     <Col sm={4} className="col1">
-                        <div>
+
+                        <Typography>
                         Bonjour, je suis
-                        </div>
-                        <div>
+                        </Typography>
+                        
+                        <Typography>
                         Développeur Web.
-                        </div>
-                        <div>
+                        </Typography>
+
+                        <Typography>
                         AMRI Maher
-                        </div>
-                        <div>
+                        </Typography>
+
+                        <Typography>
                         Développeur Web, je suis à votre disposition pour répondre à tout type de projets de création de sites internet vitrine, e-commerce, de développement spécifique ou d’applications web. 
-                        </div>
+                        </Typography>
+
+
                     </Col>
+
                     <Col sm={8} className="col2">
-                        J'ai réalisé plusieurs projets en Front-End et en Back-End, notamment de l'intégration web, du responsive ainsi que de l'animation web,  je suis à votre disposition pour répondre à tout type de projets de création de sites internet vitrine, e-commerce, de développement spécifique ou d’applications web.  
+
+                        <Typography variant='body1'>
+                            J'ai réalisé plusieurs projets en Front-End et en Back-End, notamment de l'intégration web, du responsive ainsi que de l'animation web,  je suis à votre disposition pour répondre à tout type de projets de création de sites internet vitrine, e-commerce, de développement spécifique ou d’applications web.  
+                        </Typography>
+                    
                     </Col>
+
                 </Row>
             
             </Container>
